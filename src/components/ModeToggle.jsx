46066d534db8fb0b608f7f4a83fc620b0f3baeef
Guardianprime <okongordian@gmail.com>
2025-02-24 11:30:59 +0100

    mobile main menu css styling (light-mode)

---
+++ src/components/ModeToggle.jsx
@@ -0,0 +1,13 @@
+function ModeToggle() {
+    return (
+        <div>
+        <div><img alt="sun icon"/></div>
+            <div className="toggle-button"></div>
+        <div><img alt="moon icon"/></div>
+        </div>
+    )
+}
+
+export default ModeToggle;
+
+
--- src/components/QuizMenu.jsx
+++ src/components/QuizMenu.jsx
@@ -9,23 +9,34 @@ function QuizMenu({ setTopic }) {
 
   return (
     <div className="container">
-      <h1>Welcome to the Frontend Quiz!</h1>
-      <p>Pick a subject to get started.</p>
+      <div className="heading-container">
+        <h2>Welcome to the </h2>
+        <h1>Frontend Quiz!</h1>
+        <p>Pick a subject to get started.</p>
+      </div>
       <ul>
         <li onClick={() => handleTopic("HTML")}>
-          <img alt="html icon" src="/assets/images/icon-html.svg" />
+          <div className="main-image--container one">
+            <img alt="html icon" src="/assets/images/icon-html.svg" />
+          </div>
           <span>HTML</span>
         </li>
         <li onClick={() => handleTopic("CSS")}>
-          <img alt="css icon" src="/assets/images/icon-css.svg" />
+          <div className="main-image--container two">
+            <img alt="css icon" src="/assets/images/icon-css.svg" />
+          </div>
           <span>CSS</span>
         </li>
         <li onClick={() => handleTopic("Javascript")}>
-          <img alt="js icon" src="/assets/images/icon-js.svg" />
+          <div className="main-image--container three">
+            <img alt="js icon" src="/assets/images/icon-js.svg" />
+          </div>
           <span>JavaScript</span>
         </li>
         <li onClick={() => handleTopic("Accessibility")}>
-          <img alt="accessibility icon" src="/assets/images/icon-accessibility.svg" />
+          <div className="main-image--container four">
+            <img alt="accessibility icon" src="/assets/images/icon-accessibility.svg" />
+          </div>
           <span>Accessibility</span>
         </li>
       </ul>
