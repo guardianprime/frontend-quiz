000003c7b9c108e3ef0e41ad8d6d631a3c1174af
Guardianprime <okongordian@gmail.com>
2025-03-01 11:14:47 +0100

    feature: darkmode npm react switch added.

--- src/components/ModeToggle.jsx
+++ src/components/ModeToggle.jsx
@@ -1,19 +1,34 @@
+import ReactSwitch from "react-switch";
+import { useState } from "react";
+
 
 function ModeToggle() {
+    const [theme, setTheme] = useState("light");
 
+    const toggleTheme = () => {
+        setTheme((curr) => (curr === "light" ? "dark" : "light"));
+    }
 
     return (
         <div className="modeToggle-container">
             <div>
-                <img alt="sun icon" src="assets/images/icon-sun-dark.svg" />
+                <img alt="sun icon" src={theme === "light" ? "assets/images/icon-sun-dark.svg" : "assets/images/icon-sun-light.svg"} />
             </div>
+            <ReactSwitch
+                onChange={toggleTheme}
+                checked={theme === "dark"}
+                onColor="#ffffff"
+                onHandleColor="#020202"
+                offHandleColor="#ffffff"
+                offColor="#020202"
+                height={40}
+                width={80}
+            />
             <div>
-            </div>
-            <div>
-                <img alt="moon icon" src="assets/images/icon-moon-dark.svg" />
+                <img alt="moon icon" src={theme === "light" ? "assets/images/icon-moon-dark.svg" : "assets/images/icon-moon-light.svg"} />
             </div>
         </div>
-    )
+    );
 }
 
 export default ModeToggle;
--- src/components/QuizQuestionEnd.jsx
+++ src/components/QuizQuestionEnd.jsx
@@ -24,27 +24,23 @@ function QuizQuestionEnd({ setNext, setTopic, score, setScore, questionTopic, qu
         </div>
         <ModeToggle />
       </div>
-      <div className="wrapper-end">
-        <div className="heading-container">
-          <h2 className="center">Quiz completed</h2>
-          <h1 className="center">You scored..</h1>
-        </div>
-        <div className="score-wrapper">
-          <div className="score-container">
-            <div className="image-container__flex">
-              <div className={`main-image--container ${imageClass[questionTopic]}`}>
-                {questionArray && questionArray.icon && (
-                  <img alt="topic icon" src={questionArray.icon} />
-                )}
-              </div>
-              <span>{questionTopic}</span>
-            </div>
-            <span className="score">{score}</span>
-            <p>out of 10</p>
-          </div>
-          <button onClick={handleReset}>Play Again</button>
-        </div>
+      <div className="heading-container">
+        <h2 className="center">Quiz completed</h2>
+        <h1 className="center">You scored..</h1>
       </div>
+      <div className="score-container">
+        <div className="image-container__flex">
+          <div className={`main-image--container ${imageClass[questionTopic]}`}>
+            {questionArray && questionArray.icon && (
+              <img alt="topic icon" src={questionArray.icon} />
+            )}
+          </div>
+          <span>{questionTopic}</span>
+        </div>
+        <span className="score">{score}</span>
+        <p>out of 10</p>
+      </div>
+      <button onClick={handleReset}>Play Again</button>
     </div>
   );
 }
